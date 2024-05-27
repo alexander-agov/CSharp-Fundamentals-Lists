932a0a5ed72d401c1f1d7ee245e18038e8399ceb
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-27 23:14:37 +0300

    Add files via upload

---
+++ TakeSkipRope/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+namespace TakeSkipRope
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string args = Console.ReadLine();
+            List<int> numberList = new List<int>();
+            List<string> stringList = new List<string>();
+            List<int> takeList = new List<int>();
+            List<int> skipList = new List<int>();
+            string newList = "";
+            foreach (char c in args)
+            {
+                if (char.IsDigit(c))
+                {
+                    numberList.Add(int.Parse(c.ToString()));
+                }
+                else
+                {
+                    stringList.Add(c.ToString());
+                }
+            }
+            for (int i = 0; i < numberList.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    takeList.Add(numberList[i]);
+                }
+                else
+                {
+                    skipList.Add(numberList[i]);
+                }
+            }
+            int index = 0;
+            for (int i = 0; i < takeList.Count; i++)
+            {
+                List<string> temp = new List<string>(stringList);
+                stringList.RemoveRange(0, index);
+                temp = stringList.Take(takeList[i]).ToList();
+                foreach (string s in temp)
+                {
+                    newList += s;
+                }
+                index = takeList[i] + skipList[i];
+            }
+            Console.WriteLine(newList);
+        }
+    }
+}
+/*
+ for (int i = 0; i < takeList.Count; i++)
+    {
+        List<string> temp = new List<string>(stringList);
+        temp = temp.Skip(index).Take(takeList[i]).ToList();
+        foreach (string s in temp)
+        {
+            newList += s;
+        }
+        index = takeList[i] + skipList[i];
+    }
+*/
+
+
---
+++ Train/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Train
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int maxCapacity = int.Parse(Console.ReadLine());
+            string command = string.Empty;
+            int findElement = -1;
+            int index = -1;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] input = command.Split();
+                if (input[0] == "Add" && int.Parse(input[1]) <= maxCapacity)
+                {
+                    wagons.Add(int.Parse(input[1]));
+                }
+                else
+                {
+                    findElement = wagons.Find(n => n + int.Parse(input[0]) <= maxCapacity);
+                    if (findElement + int.Parse(input[0]) <= maxCapacity)
+                    {
+                        index = wagons.IndexOf(findElement);
+                        wagons[index] += int.Parse(input[0]);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
+
