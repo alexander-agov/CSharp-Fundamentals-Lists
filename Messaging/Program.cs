86fcffe93ec475b6b977cb68b5946526bedbd5da
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-27 23:08:25 +0300

    Add files via upload

---
+++ Messaging/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Messaging
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] sequence = Console.ReadLine().Split();
+            string text = Console.ReadLine().Trim();
+            string message = "";
+
+            while (sequence.Length > 0)
+            {
+                int sum = 0;
+                foreach (char digit in sequence[0])
+                {
+                    sum += int.Parse(digit.ToString());
+                }
+                int index = sum % text.Length;
+                message += text[index];
+                text = text.Remove(index, 1);
+                sequence = sequence.Skip(1).ToArray();
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
