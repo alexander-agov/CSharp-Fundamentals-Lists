2ccc0b5692ff629c511c56ec8306a893f72d6e27
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-27 22:56:24 +0300

    Add files via upload

---
+++ CarRace/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace CarRace
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> race = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int half = race.Count / 2;
+            List<int> leftSide = race.GetRange(0, half);
+            List<int> rightSide = race.GetRange(half + 1, half);
+            double leftSum = 0;
+            double rightSum = 0;
+            if (!leftSide.Contains(0))
+            {
+                leftSum += leftSide.Sum();
+            }
+            else
+            {
+                int findIndex = leftSide.FindIndex(x => x == 0);
+                for (int j = 0; j < findIndex; j++)
+                {
+                    leftSum += leftSide[j] * 0.8;
+                }
+                for (int k = findIndex + 1; k < leftSide.Count(); k++)
+                {
+                    leftSum += leftSide[k];
+                }
+            }
+
+            if (!rightSide.Contains(0))
+            {
+                rightSum += rightSide.Sum();
+            }
+            else
+            {
+                int findIndex = rightSide.FindIndex(x => x == 0);
+                for (int j = rightSide.Count - 1; j > 0; j--)
+                {
+                    rightSum += rightSide[j] * 0.8;
+                }
+                for (int k = findIndex - 1; k >= 0; k--)
+                {
+                    rightSum += rightSide[k];
+                }
+            }
+            if (leftSum > rightSum)
+            {
+                Console.WriteLine($"The winner is right with total time: {FormatResult(rightSum)}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is left with total time: {FormatResult(leftSum)}");
+            }
+        }
+        static string FormatResult(double result)
+        {
+            if (result % 1 == 0)
+            {
+                return ((int)result).ToString();
+            }
+            else
+            {
+                return result.ToString("f1");
+            }
+        }
+    }
+}
---
+++ CardsGame/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace CardsGame
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> firstPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+            
+
+            int minCount = Math.Min(firstPlayer.Count, secondPlayer.Count);
+            while (minCount > 0)
+            {
+                if (firstPlayer[0] == secondPlayer[0])
+                {
+                    firstPlayer.RemoveAt(0);
+                    secondPlayer.RemoveAt(0);
+                }
+                else if (firstPlayer[0] > secondPlayer[0])
+                {
+                    firstPlayer.Add(secondPlayer[0]);
+                    firstPlayer.Add(firstPlayer[0]);
+                    secondPlayer.RemoveAt(0);
+                    firstPlayer.RemoveAt(0);
+                }
+                else
+                {
+                    secondPlayer.Add(firstPlayer[0]);
+                    firstPlayer.RemoveAt(0);
+                    secondPlayer.Add(secondPlayer[0]);
+                    secondPlayer.RemoveAt(0);
+                }
+                minCount = Math.Min(firstPlayer.Count, secondPlayer.Count);
+            }
+            if (firstPlayer.Count > 0)
+            {
+                Console.WriteLine($"First player wins! Sum: {firstPlayer.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Second player wins! Sum: {secondPlayer.Sum()}");
+            }
+        }
+    }
+}
