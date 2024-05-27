2aad7c43e3f4476643c011a28b4b68942d388782
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-27 23:07:42 +0300

    Add files via upload

---
+++ ListOfProducts/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace ListOfProducts
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+            products.Sort();
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
---
+++ MergingLists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace MergingLists
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> firstList = ReadNumbers();
+            List<int> secondList = ReadNumbers();
+
+            List<int> result = new List<int>();
+
+            int greatererCount = Math.Max(firstList.Count, secondList.Count);
+            for (int i = 0; i < greatererCount; i++)
+            {
+                if (i < firstList.Count)
+                {
+                    result.Add(firstList[i]);
+                }
+                if (i < secondList.Count)
+                {
+                    result.Add(secondList[i]);
+                }
+            }
+            PrintResult(result);
+        }
+        static List<int> ReadNumbers()
+        {
+            return Console.ReadLine().Split().Select(int.Parse).ToList();
+        }
+        static void PrintResult(List<int> numbers, string separator = " ")
+        {
+            Console.WriteLine(string.Join(separator, numbers));
+        }
+    }
+}
