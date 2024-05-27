12db10c25b6c6c5027309f176c1830f4f87231a6
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-27 23:09:32 +0300

    Add files via upload

---
+++ RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> positiveNumbers = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] >= 0)
+                {
+                    positiveNumbers.Add(numbers[i]);
+                }
+            }
+            if (positiveNumbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+            positiveNumbers.Reverse();
+            Console.WriteLine(string.Join(" ", positiveNumbers));
+        }
+    }
+}
+
+
